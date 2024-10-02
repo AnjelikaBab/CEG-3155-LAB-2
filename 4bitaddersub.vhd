1606a7f722a92bfa97817370ffee9a141fef24ac
AnjelikaBab <banjelika6@gmail.com>
2024-10-02 18:33:10 -0400

    turning to 4 bit

--- 4bitaddersub.vhd
+++ 4bitaddersub.vhd
@@ -1,16 +1,16 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-ENTITY threeBitAdderSubtractor IS
+ENTITY fourBitAdderSubtractor IS
 	PORT(
-		i_Ai, i_Bi     : IN  STD_LOGIC_VECTOR(2 downto 0);
+		i_Ai, i_Bi     : IN  STD_LOGIC_VECTOR(3 downto 0);
 		operationFlag  : IN  STD_LOGIC;
 		o_CarryOut     : OUT STD_LOGIC;
-		o_Sum          : OUT STD_LOGIC_VECTOR(2 downto 0));
-END threeBitAdderSubtractor;
+		o_Sum          : OUT STD_LOGIC_VECTOR(3 downto 0));
+END fourBitAdderSubtractor;
 
-ARCHITECTURE rtl OF threeBitAdderSubtractor IS
-	SIGNAL int_Sum, int_CarryOut : STD_LOGIC_VECTOR(2 downto 0);
+ARCHITECTURE rtl OF fourBitAdderSubtractor IS
+	SIGNAL int_Sum, int_CarryOut : STD_LOGIC_VECTOR(3 downto 0);
 
 	COMPONENT oneBitAdderSubtractor
 	PORT(
@@ -21,7 +21,16 @@ ARCHITECTURE rtl OF threeBitAdderSubtractor IS
 	END COMPONENT;
 
 BEGIN
-	-- Instantiation for the most significant bit (bit 2)
+	-- Instantiation for the most significant bit (bit 3)
+	add3: oneBitAdderSubtractor
+	PORT MAP (i_CarryIn => int_CarryOut(2), -- Connect carry from bit 2
+			  operationFlag => operationFlag,
+			  i_Ai => i_Ai(3),
+			  i_Bi => i_Bi(3),
+			  o_Sum => int_Sum(3),
+			  o_CarryOut => int_CarryOut(3));
+
+	-- Instantiation for bit 2
 	add2: oneBitAdderSubtractor
 	PORT MAP (i_CarryIn => int_CarryOut(1), -- Connect carry from bit 1
 			  operationFlag => operationFlag,
@@ -50,6 +59,6 @@ BEGIN
 
 	-- Output Driver
 	o_Sum <= int_Sum;
-	o_CarryOut <= int_CarryOut(2); -- Carry-out from the most significant bit
+	o_CarryOut <= int_CarryOut(3); -- Carry-out from the most significant bit
 
 END rtl;
